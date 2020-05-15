2ddcb6427bd8491305579173c950d93f8802a6b4
Sarwar Ali <47899529+sarwar1227@users.noreply.github.com>
2020-05-15 15:55:08 +0530

    Initial Files Uploaded

---
+++ test.js
@@ -0,0 +1,21 @@
+var button=document.getElementById("enter");
+var input=document.getElementById("userinput");
+var ul=document.querySelector("ul");
+function inputlength(){
+	return input.value.length;
+}
+function createlistelement()
+{
+	var li=document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value=""
+}
+button.addEventListener("click",function(){
+	if(inputlength()>0)
+		createlistelement()
+})
+input.addEventListener("keypress",function(event){
+	if(inputlength()>0&&event.keyCode===13)
+		createlistelement()
+})
